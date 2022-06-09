983b99f985f32a14f127946d5d007b369261cb82
Super Zombi <rost201541@gmail.com>
2022-06-09 15:43:10 +0300

    Update about.js

--- GICutscenesUI/web/scripts/about.js
+++ GICutscenesUI/web/scripts/about.js
@@ -4,6 +4,8 @@ async function load_about_info(){
 		info_loaded = true;
 		let script_ver = await eel.get_GICutscenes_ver()();
 		let ffmpeg_ver = await eel.get_ffmpeg_ver()();
+		let latest_ui_ver = await eel.get_latest_ui_version()();
+		let latest_script_ver = await eel.get_latest_script_version()();
 
 		if (script_ver){
 			document.getElementById("script_ver").innerHTML = script_ver
@@ -15,10 +17,44 @@ async function load_about_info(){
 				document.getElementById("ffmpeg_year").innerHTML = ffmpeg_ver.year
 			}
 		}
+
+		if (latest_ui_ver){
+			if ( is_this_new_ver(APP_VERSION, latest_ui_ver) ){
+				let el = document.createElement('code')
+				el.className = "update"
+				el.title = LANG("new_update")
+				el.setAttribute("translation", "__title:new_update__")
+				el.innerHTML = latest_ui_ver
+				document.getElementById("info_version").parentNode.innerHTML += "<a class='arrow'></a>"
+				document.getElementById("info_version").parentNode.appendChild(el)
+			}
+		}
+		if (latest_script_ver){
+			if ( is_this_new_ver(script_ver, latest_script_ver) ){
+				let el = document.createElement('code')
+				el.className = "update"
+				el.title = LANG("new_update")
+				el.setAttribute("translation", "__title:new_update__")
+				el.innerHTML = latest_script_ver
+				document.getElementById("script_ver").parentNode.innerHTML += "<a class='arrow'></a>"
+				document.getElementById("script_ver").parentNode.appendChild(el)
+			}
+		}
+		
 		document.getElementById("loading").style.display = "none"
 	}
 }
 function allow_reload_info(){
 	info_loaded = false;
 	document.getElementById("loading").style.display = "block"
+}
+
+function is_this_new_ver(old_ver, new_ver){
+	function clean(string){
+		return string.replace(/[^0-9. ]/g, "")
+	}
+	if (clean(old_ver) != clean(new_ver)){
+		return true;
+	}
+	return false;
 }
